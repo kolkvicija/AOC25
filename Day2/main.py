028dc00821675118437feaac0f2130faa894b544
kolkvicija <vaka.vice@gmail.com>
2025-12-02 15:31:43 +0100

    Day 2, Part 2; Day 2 finished.

--- Day2/main.py
+++ Day2/main.py
@@ -2,6 +2,7 @@ with open("input.txt") as file:
     f = file.read()
 all_ranges = f.split(",")
 
+
 def part_one():
     invalid_ids = 0
 
@@ -10,20 +11,41 @@ def part_one():
         start = rang[0]
         end = rang[1]
 
-        for num in range(int(start), int(end)+1):
-            l = len(str(num))
-            if l % 2 == 0:
-                first = str(num)[0:l//2]
-                second = str(num)[l//2:l]
+        for num in range(int(start), int(end) + 1):
+            length = len(str(num))
+            if length % 2 == 0:
+                first = str(num)[0:length // 2]
+                second = str(num)[length // 2:length]
                 if first == second:
                     invalid_ids += num
 
     print(invalid_ids)
 
-def part_two():
-    pass
 
+def part_two():
+    def is_repeating_pattern(number):
+        s = str(number)
+        n = len(s)
+        for x in range(1, n // 2 + 1):
+            if n % x == 0:
+                if s[:x] * (n // x) == s:
+                    return True
+        return False
+
+    invalid_ids = 0
+
+    for i in all_ranges:
+        rang = i.split("-")
+        start = int(rang[0])
+        end = int(rang[1])
+
+        for num in range(start, end + 1):
+            if is_repeating_pattern(num):
+                invalid_ids += num
+
+    print(invalid_ids)
 
 
 if __name__ == "__main__":
-    part_one()
+    #part_one()
+    part_two()
