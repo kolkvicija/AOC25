ac4240ee090379b8bf7ed0cc4ee3f4a5e5cee0a3
kolkvicija <vaka.vice@gmail.com>
2025-12-05 09:52:19 +0100

    Day 5 - both parts finished

--- Day5/main.py
+++ Day5/main.py
@@ -4,20 +4,20 @@ with open("input.txt") as file:
 ranges = []
 indexes = []
 
+for line in f:
+    if "-" in line:
+
+        l, r = line.split("-")
+        ranges.append((int(l), int(r)))
+    else:
+        line = line.replace("\n", "").replace("\r", "")
+        if line:
+            indexes.append(int(line))
+
+
 def part_one():
     counter = 0
     fl = 0
-
-    for line in f:
-        if "-" in line:
-
-            l, r = line.split("-")
-            ranges.append((int(l), int(r)))
-        else:
-            line = line.replace("\n", "").replace("\r", "")
-            if line:
-                indexes.append(int(line))
-
 
     for idx in indexes:
         fl = 0
@@ -29,5 +29,29 @@ def part_one():
 
     print(counter)
 
+
+def part_two():
+    total = 0
+    ranges.sort(key=lambda x: x[0])
+
+    m = []
+    for start, end in ranges:
+
+        if not m:
+            m.append((start, end))
+
+        else:
+            last_start, last_end = m[-1]
+            if start <= last_end + 1:
+                m[-1] = (last_start, max(last_end, end))
+            else:
+                m.append((start, end))
+
+    total = sum(end - start + 1 for start, end in m)
+
+    return total
+
+
 if __name__ == "__main__":
-    part_one()
+    # part_one()
+    print(part_two())
